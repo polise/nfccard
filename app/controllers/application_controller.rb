c4a3f36b27092e31c1a4c48c771bdb3cffbe417b
Alexandre Polise <alexpolise@gmail.com>
2015-09-10 15:28:55 +0100

    geolocation debugging 2.1.6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,13 +15,13 @@ class ApplicationController < ActionController::Base
   	if cookies[:lat_lng]
 	  	@lat_lng = cookies[:lat_lng].split("|")
   		logger.debug("at lat_long = #{@lat_lng}")
-  	#elsif
-    #  @lat_lng = lat_lng_via_geolocation
+  	elsif
+      @lat_lng = lat_lng_via_geolocation
     end
   end
 
 
-  # def lat_lng_via_geolocation
-  #   @lat_lng ||= session[:lat_lng] ||=  application.js { render :action => "getGeoLocation", :layout => false }
-  # end
+  def lat_lng_via_geolocation
+    @lat_lng  = getGeoLocation()
+  end
 end
